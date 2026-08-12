23ec19f717af207f57b345bc7273897444f2fb3b
Nytreon <nytreon.inc@protonmail.com>
2026-08-12 16:19:10 -0300

    Version 1.0.0-00
    Todos os arquivos necessários da Nytreon foram adicionados a este repositório. Aqui estão reunidos os documentos, informações, termos, políticas e demais arquivos relacionados à organização e aos seus projetos. Este espaço mantém os principais recursos da Nytreon centralizados, organizados e disponíveis para consulta.

---
+++ scripts/copyright.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+const copy = window.document.getElementById('copyright');
+
+copy.innerHTML = `&copy; 2026 - ${currentYear} <strong>Nytreon</strong>. Todos os direitos reservados.`;
